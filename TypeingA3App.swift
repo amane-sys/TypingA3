5c910ee9c17f50ea6ce80f0e2b5cba1c6d685027
Tianao <tianao.jia@ganjing.dev>
2025-03-06 00:22:23 -0500

    init

---
+++ TypeingA3App.swift
@@ -0,0 +1,17 @@
+//
+//  TypeingA3App.swift
+//  TypeingA3
+//
+//  Created by Adelaide Jia on 2025/03/06.
+//
+
+import SwiftUI
+
+@main
+struct TypeingA3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
